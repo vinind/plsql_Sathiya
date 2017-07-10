687e8deccdc8f6af4388630c1f71b8d11802b5d3
kasathiya <kasseelan@gmail.com>
2017-07-09 19:02:10 -0700

    customer table insert data

---
+++ src_customer_insert.sql
@@ -0,0 +1,4 @@
+insert into customer(customer_id,first_name,last_name,phone,email,address)
+values(customer_seq.nextval,'peter','hein',8569587894,'peterhein','bangalore');
+insert into customer(customer_id,first_name,last_name,phone,email,address)
+values(customer_seq.nextval,'mahesh','babu',5689874521,'maheshbabu','kerala');
