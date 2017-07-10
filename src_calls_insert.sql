6b13c6648710a8174be537418ac7b23fd2b8cf64
kasathiya <kasseelan@gmail.com>
2017-07-10 12:33:41 -0700

    calls table insert script

---
+++ src_calls_insert.sql
@@ -0,0 +1,4 @@
+insert into calls(call_id,order_id,employee_id,status,reason)
+values(calls_seq.nextval,1,100,'Success','Good');
+insert into calls(call_id,order_id,employee_id,status,reason)
+values(calls_seq.nextval,2,300,'Postponed','Time');
