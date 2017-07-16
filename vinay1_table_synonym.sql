7d5aba1a9c6b2b3395f03f636067f36d2ba22c59
kasathiya <kasseelan@gmail.com>
2017-07-16 08:39:28 -0700

    public synonym for vinay table

---
+++ vinay1_table_synonym.sql
@@ -0,0 +1,5 @@
+create public synonym regions for sathiya.regions;
+create public synonym countries for sathiya.countries;
+create public synonym locations for sathiya.locations;
+create public synonym employees for sathiya.employees;
+create public synonym departments for sathiya.departments;
