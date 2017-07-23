d4013f7ab1000bf322ef18b43a6b1fca0f7f39c3
kasathiya <kasseelan@gmail.com>
2017-07-22 22:09:02 -0700

    items trigger

--- items_trigger.sql
+++ items_trigger.sql
@@ -1,8 +1,8 @@
 create or replace trigger items_trig 
-before insert or update on items
+before insert on items
 for each row
 begin
-if :new.item_id=null then
+if :new.item_id is null then
 select items_sequence.nextval 
 into :new.item_id
 from dual;
